52554a855003ddc9c6f23e3868f54ffd7871753e
venturions <alex.eduardo10@hotmail.com>
2023-06-16 19:59:12 -0300

    adição da status bar estilizada

--- App.tsx
+++ App.tsx
@@ -9,12 +9,18 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 import { Loading } from '@components/Loading'
+import { StatusBar } from 'react-native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   )
