0819f869df00c6b959ea30941ddbbd9a28d5909a
venturions <alex.eduardo10@hotmail.com>
2023-06-19 23:41:22 -0300

    Criando Componente PlayerCard

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,10 +6,11 @@ import { Input } from '@components/Input'
 import { Filter } from '@components/Filter'
 import { FlatList } from 'react-native'
 import { useState } from 'react'
+import { PlayerCard } from '@components/PlayerCard'
 
 export default function Players() {
   const [team, setTeam] = useState('Time A')
-  const [players, setPlayers] = useState([])
+  const [players, setPlayers] = useState(['Alex', 'Diego', 'Rodrigo'])
 
   return (
     <Container>
@@ -38,6 +39,13 @@ export default function Players() {
         />
         <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
       </HeaderList>
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+      />
     </Container>
   )
 }
