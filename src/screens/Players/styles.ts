2d03a4cb2891a7a83de197e05ee03b74ea6ab2b8
venturions <alex.eduardo10@hotmail.com>
2023-06-22 20:25:50 -0300

    Utilizando a SafeAreaView

--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,7 +1,8 @@
 import { View, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { css, styled } from 'styled-components'
 
-export const Container = styled(View)`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
 
