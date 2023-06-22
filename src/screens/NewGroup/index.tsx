b214a305f01acc034f7d28450a2f7b04b31c411d
venturions <alex.eduardo10@hotmail.com>
2023-06-22 20:31:21 -0300

    Passando parâmetros pela rota

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,12 +4,15 @@ import { Highlight } from '@components/Highlight'
 import { Button } from '@components/Button'
 import { Input } from '@components/Input'
 import { useNavigation } from '@react-navigation/native'
+import { useState } from 'react'
 
 export default function NewGroup() {
+  const [groupName, setGroupName] = useState('')
+
   const navigation = useNavigation()
 
   function handleNew() {
-    navigation.navigate('players', { group: 'Galera da Rocket' })
+    navigation.navigate('players', { group: groupName })
   }
 
   return (
@@ -21,7 +24,7 @@ export default function NewGroup() {
           title="Nova turma"
           subtitle="crie a turma para adicionar pessoas"
         />
-        <Input placeholder="Nome da turma" />
+        <Input placeholder="Nome da turma" onChangeText={setGroupName} />
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
