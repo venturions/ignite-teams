d67436885a094fb535329386f496a4f8453429d8
venturions <alex.eduardo10@hotmail.com>
2023-06-24 19:53:57 -0300

    Tratando exceções

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -6,6 +6,8 @@ import { Input } from '@components/Input'
 import { useNavigation } from '@react-navigation/native'
 import { useState } from 'react'
 import { groupCreate } from '@storage/group/groupCreate'
+import { AppError } from '@utils/AppError'
+import { Alert } from 'react-native'
 
 export default function NewGroup() {
   const [groupName, setGroupName] = useState('')
@@ -14,10 +16,19 @@ export default function NewGroup() {
 
   async function handleNew() {
     try {
+      if (groupName.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.')
+      }
+
       await groupCreate(groupName)
       navigation.navigate('players', { group: groupName })
     } catch (err) {
-      console.log(err)
+      if (err instanceof AppError) {
+        Alert.alert('Novo Grupo', err.message)
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possivel criar um novo grupo.')
+        console.log(err)
+      }
     }
   }
 
